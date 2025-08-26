fa4725fcefa68e6e2663a9de17b3eb15357e523b
Namit Chaudhary <namitchaudhary7248@gmail.com>
2025-08-26 17:01:23 +0530

    Setup for Vercel deployment

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ require('dotenv').config();
 
 const app = express()
 const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.use(cors());
 app.use(bodyParser.json());
